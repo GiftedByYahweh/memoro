3a406cfe8a95c26af51e86688f209de81badb924
GiftedByYahweh <giftedbyyahweh@gmail.com>
2026-09-20 00:02:17 +0300

    feat: remove form title

--- apps/client/src/components/features/auth/AuthStepLayout.vue
+++ apps/client/src/components/features/auth/AuthStepLayout.vue
@@ -22,12 +22,6 @@ const emit = defineEmits<{
   <div class="step-container">
     <div class="step-header">
       <AppBackButton v-if="showBack" @click="emit('back')" />
-      <AppText variant="h3" color="primary">{{ title }}</AppText>
-      <slot name="description">
-        <AppText v-if="description" variant="body-sm" color="secondary">
-          {{ description }}
-        </AppText>
-      </slot>
     </div>
 
     <slot />
