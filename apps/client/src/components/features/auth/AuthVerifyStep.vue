c189ceb7ce6cd1b9f09ff0abd67c94659337a154
GiftedByYahweh <giftedbyyahweh@gmail.com>
2026-09-20 01:52:51 +0300

    fix: fix variant

--- apps/client/src/components/features/auth/AuthVerifyStep.vue
+++ apps/client/src/components/features/auth/AuthVerifyStep.vue
@@ -118,10 +118,10 @@ function onResend(): void {
       </AppButton>
 
       <div class="resend-action">
-        <AppButton v-if="timer > 0" variant="outline" size="lg" block disabled>
+        <AppButton v-if="timer > 0" variant="secondary" size="lg" block disabled>
           {{ t('auth.resendCodeIn', { seconds: timer }) }}
         </AppButton>
-        <AppButton v-else variant="outline" size="lg" block @click="onResend">
+        <AppButton v-else variant="secondary" size="lg" block @click="onResend">
           {{ t('auth.resendCodeAction') }}
         </AppButton>
       </div>
